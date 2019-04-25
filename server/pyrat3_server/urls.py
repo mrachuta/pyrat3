329fcbc95ede4d9040669c0a3db955b5db36c3b4
Mateusz Rachuta <mrachuta@thinkbox.pl>
2019-04-25 13:43:18 +0200

    Changed naming convention (variables) from command to job

--- server/pyrat3_server/urls.py
+++ server/pyrat3_server/urls.py
@@ -7,7 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     url(r'^index/$', views.Index.as_view(), name='index'),
     url(r'^client_table/$', views.ClientTable.as_view(), name='client_table'),
-    url(r'^generate_command_id/$', views.generate_command_id, name='generate_command_id'),
+    url(r'^generate_job_id/$', views.generate_job_id, name='generate_job_id'),
+    path('client_files/<pk>/', views.client_files, name='client_files'),
     path('api/', views.ClientList.as_view(), name='client_list'),
     path('api/<pk>/', views.ClientDetail.as_view(), name='client_details'),
     path('api/<pk>/upload/', csrf_exempt(views.ClientUploadFile.as_view()), name='client_upload_file')
